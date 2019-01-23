da95b108c191ba22363a4d26372521d39cb14f2d
Federico Brigante <github@bfred.it>
2019-01-23 15:55:37 +0700

    Escape innerHTML

--- source/content.js
+++ source/content.js
@@ -1,5 +1,14 @@
 "use strict";
 
+function escapeHTML(input) { 
+  return input
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+}
+
 function init() {
   let fileActions = document.querySelector(".file-header .file-actions");
 
@@ -65,7 +74,7 @@ function showPrivateMessage(dropdownElement) {
 
 function showTooManyPRsMessage(dropdownElement, numberOfPRs) {
   dropdownElement.innerHTML = `
-    <p>There are <strong>${numberOfPRs}</strong> PRs in this repository. Requesting all of them at once can cause performance issues on your browser.</p>
+    <p>There are <strong>${escapeHTML(numberOfPRs)}</strong> PRs in this repository. Requesting all of them at once can cause performance issues on your browser.</p>
     <p>Because of this and due to the fact that this extension is in an early <strong><i>alpha</i></strong> release, it is currently limited to repositories with less than 10 PRs.</p>
     <p>If you are interested in knowing more, check out the progress of this feature on 
     its <a href="https://github.com/dzhavat/potential-changes" target="_blank">repo</a>.</p>
@@ -125,7 +134,7 @@ function checkForPotentialChanges(dropdownElement) {
     .catch(error => {
       dropdownElement.innerHTML = `
         <p>There was a problem when trying to get the list of pull requests.</p>
-        <p>Error: ${error.message}.</p>
+        <p>Error: ${escapeHTML(error.message)}.</p>
       `;
     });
 }
